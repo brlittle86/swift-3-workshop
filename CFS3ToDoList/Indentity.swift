b6e5cb5abb11234f86036b1a0e2b33e8c082a7d2
Brandon Little <brlittle86@gmail.com>
2017-03-12 13:11:50 -0700

    Learned about using the MVC file structure to create and edit a quick Todo List app, including creation and manipulation of UI elements.

---
+++ CFS3ToDoList/Indentity.swift
@@ -0,0 +1,13 @@
+//
+//  Indentity.swift
+//  CFS3ToDoList
+//
+//  Created by Brandon Little on 3/12/17.
+//  Copyright © 2017 Adam Wallraff. All rights reserved.
+//
+
+import Foundation
+
+protocol Identity {
+    var indentifier: String { set get }
+}
